2b433efa2fb666d1969ef44bbf4f99858bee5f89
zidan_1013 <80787878+zidan4@users.noreply.github.com>
2026-01-11 22:45:21 +0300

    Update express.mjs

--- express.mjs
+++ express.mjs
@@ -1,1 +1,31 @@
 
+import express from "express";
+
+const app = express();
+app.use(express.json()):
+
+//put updates every row in the database
+app.put("users", (req, res) => {
+  const { body, params:{id} } = req;
+  const parsedId = parseInt(id)
+  if(isNaN){
+    return res.sendStatus(400);
+  };
+  const foundUserIndex = users.findIndex( user => { user.id === id });
+  if(foundUserIndex === -1 ) {
+    return res.sendStatus(400);
+  };
+  users[foundUserIndex] = { id: parsedId, ...body }
+  return res.status(200);
+});
+
+//app.patch updates the database partially
+app.patch("user", (req, res) => {
+  const { body, params:{id} } = req;
+  
+});
+
+const port = process.env.PORT || 3000;
+app.listen( port, (req, res) => {
+  console.log("done")
+});
